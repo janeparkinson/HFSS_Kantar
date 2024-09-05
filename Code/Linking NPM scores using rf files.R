97b2287853d71473397ffed6cd9410c67bb0627f
clfraser <catriona.fraser2@phs.scot>
2024-09-05 14:55:48 +0100

    Changes with new HFSS itemised file from Kantar

--- Code/Linking NPM scores using rf files.R
+++ Code/Linking NPM scores using rf files.R
@@ -34,16 +34,16 @@ rf_combined <- read_parquet("rf_markets_combined.parquet")
 npm <- fread("NPM_2024P2.csv")
 
 # HFSS category data
-hfss_2023 <- read_xlsx("2023 Purchase Data/HFSS.xlsx")
-hfss_2022 <- read_xlsx("2022 Purchase Data/HFSS.xlsx")
-# Check equal
-all.equal(hfss_2023, hfss_2022)
+# There is a mistake in Danish pastries and fruit/malt loaves extended being swapped.
+# Has been fixed in amended sheet.
+hfss <- read_xlsx("PHS HFSS Itemisation Breakdown amended.xlsx", range = "A2:D642")
 
 # rst_products, for longer product names
 rst_products_2023 <- fread("2023 Purchase Data/rst_products.csv")
 rst_products_2022 <- fread("2022 Purchase Data/rst_products.csv")
 # Check equal
 all.equal(rst_products_2022, rst_products_2023)
+# They are equal, so use 2023 file below
 
 ## Process NPM data
 
@@ -55,117 +55,115 @@ npm_latest <- npm %>%
 # Match NPM products to markets from rf files
 
 npm_markets <- npm_latest %>%
-  merge(rf_combined, by.x = "PRODUCT", by.y = "Product", all.x = TRUE)
+  merge(rf_combined, by.x = "PRODUCT", by.y = "Product", all.x = TRUE) %>%
+  rename(market = "RST 4 Market",
+         sub_market = "RST 4 Sub Market",
+         extended = "RST 4 Extended",
+         low_sugar = "Low Sugar/Calorie/Fat")
 
 ## Process HFSS data
 
-# Tidy HFSS data
+# Rename columns in HFSS data and concatenate market, submarket and extended
+# Only some markets/submarkets have extended specified, so only include extended in concatenation if it's relevant
 
-hfss_tidy <- hfss_2023 %>%
+hfss_tidy <- hfss %>%
   rename(category = "HFSS Category",
-         market = Filters,
-         sub_market = "...3",
-         extended = "...4") %>%
-  fill(category, .direction = "down") %>%
-  filter(!is.na(category)) # Remove first blank row
-
-# Only show submarkets that are in the HFSS categories
-
-# First create vector of submarkets in HFSS categories
-hfss_sub_markets <- hfss_tidy %>%
-  select(sub_market) %>%
-  filter(str_detect(sub_market, "RST 4", negate = TRUE))
-
-# Then use this to filter the NPM markets df
-npm_hfss_sub_market_lookup <- npm_markets %>%
-  rename(market = "RST 4 Market", # First rename columns, otherwise filtering doesn't seem to work
+         market = "RST 4 Market",
          sub_market = "RST 4 Sub Market",
          extended = "RST 4 Extended") %>%
-  filter(sub_market %in% hfss_sub_markets$sub_market) %>%
-  select(market, sub_market, extended) %>%
-  unique(.) %>%
-  arrange(market, sub_market, extended)
+  mutate(concat = if_else(is.na(extended), paste(market, sub_market), paste(market, sub_market, extended)))
 
-# Match markets in lookup to submarkets in HFSS file?
-# Then check if it's easy to match these manually
+# Create a df where extended isn't NA
 
-sub_market_to_market <- hfss_tidy %>%
-  select(sub_market) %>%
-  merge(npm_hfss_sub_market_lookup, by = "sub_market") %>%
-  arrange(market) %>%
-  select(market, everything()) # Move market column to the start of the dataframe
+hfss_extended <- hfss_tidy %>%
+  filter(!is.na(extended))
 
-# Save file as csv
-#write_csv(sub_market_to_market, "hfss_sub_market_to_market.csv")
+# Use this to set how to concatenate in the npm_markets file
 
-# Soft drinks are only subject to regulations if they have added sugar
-# So, need to match on the 'Low Sugar/Calorie/Fat' column from the rf files and exclude those that are 'sugar free' and 'no added sugar'?
-# In files rf0084. rf0205, 
-# For the audit, do we need to know whether products are in the categories and HFSS vs not in the categories and HFSS?
-
-# Alcohol substitutes are exempt according to the English legislation (https://www.legislation.gov.uk/uksi/2018/41/regulation/9/made), but non-alcoholic beer is in the Kantar categories
-
-# Read in hfss_cleaned, which has been done manually
-
-hfss_cleaned <- read_xlsx("hfss_cleaned.xlsx", sheet = "Matched")
-
-# Concatenate market, submarket and extended
-
-hfss_cleaned <- hfss_cleaned %>%
-  mutate(Extended = coalesce(Extended, ""), # Replace NAs in Extended column with blank
-         concat = paste(Market, Submarket, Extended))
-
-# Concatenate in npm_markets file
-npm_markets_concat <- npm_markets %>%
-  rename(Market = "RST 4 Market",
-         Submarket = "RST 4 Sub Market",
-         Extended = "RST 4 Extended") %>%
-  mutate(concat = paste(Market, Submarket, Extended))
+npm_markets <- npm_markets %>%
+  mutate(concat = if_else(market %in% hfss_extended$market &
+                            sub_market %in% hfss_extended$sub_market &
+                            paste(market, sub_market, extended) %in% hfss_tidy$concat,
+                          paste(market, sub_market, extended),
+                          paste(market, sub_market)))
 
 # Match on HFSS categories
-npm_markets_hfss <- npm_markets_concat %>%
-  merge(hfss_cleaned %>%
-          select(hfss_category = Category, concat),
+npm_markets_hfss <- npm_markets %>%
+  merge(hfss_tidy %>%
+          select(hfss_category = category, concat),
         by = "concat",
         all.y = TRUE)
 
-# Check which combinations of markets/submarkets/extended are missing
+# Check if any combinations of markets/submarkets/extended are missing
 npm_markets_hfss %>%
   filter(is.na(PRODUCT)) %>%
   View()
 
-# None!
+# Investigate missing ones:
+# Frozen Ready Meals Italian Frozen Ready Me Italian Kievs
+npm_markets %>%
+  filter(market == "Frozen Ready Meals",
+         sub_market == "Italian") %>%
+  group_by(extended) %>%
+  tally()
+
+# Check rf and NPM files separately
+
+rf_combined %>%
+  rename(market = "RST 4 Market",
+         sub_market = "RST 4 Sub Market",
+         extended = "RST 4 Extended") %>%
+  filter(market == "Frozen Ready Meals" &
+           sub_market == "Italian"
+         #,
+         #extended == "Frozen Ready Breaded-Kievs/E"
+         ) %>%
+  group_by(extended) %>%
+  tally()
+
+# Not in rf files
+
+# Process drinks:
+# Remove non-alcoholic beer, since SG has confirmed that most of these are exempt
+# Also remove any drinks that don't have added sugar
+
+# First, investigate added sugar categories for drinks
+npm_markets_hfss %>%
+  filter(hfss_category == "Prepared Soft Drinks") %>%
+  group_by(low_sugar) %>%
+  tally()
+
+npm_markets_hfss %>%
+  filter(hfss_category == "Prepared Soft Drinks") %>%
+  arrange(Manufacturer, Brand) %>%
+  View()
+
+# Keep only 'Regular' and 'Standard'
+# Also filter out non-alcoholic beer
+npm_markets_hfss_filtered <- npm_markets_hfss %>%
+  filter(hfss_category != "Prepared Soft Drinks" | low_sugar %in% c("Regular", "Standard"),
+         market != "Non Alcoholic Beer")
 
 # Match on full names of products from rst_products file
-npm_hfss_prod_names <- npm_markets_hfss %>%
+npm_hfss_prod_names <- npm_markets_hfss_filtered %>%
   merge(rst_products_2023%>%
           select(PRODUCT, PRODUCT_LONG_DESC),
         by = "PRODUCT",
         all.x = TRUE)
 
-# Remove duplicates and NA products
-npm_hfss_prod_names_no_dups <- distinct(npm_hfss_prod_names) %>%
-  filter(!is.na(PRODUCT))
-
 # How many HFSS vs not HFSS?
-table(npm_hfss_prod_names_no_dups$HFSS)
+table(npm_hfss_prod_names$HFSS)
 # 59% HFSS
 
 # How many don't have a long description?
-npm_hfss_prod_names_no_dups %>%
+npm_hfss_prod_names %>%
   filter(PRODUCT_LONG_DESC == "") %>%
   View()
-# 91,124 don't have long descriptions, around 36%
+# 89,044 don't have long descriptions, around 37%
 
-## Investigate alcohol substitute drinks
-npm_hfss_prod_names_no_dups %>%
-  filter(Market == "Non Alcoholic Beer") %>%
-  View()
-# 192 products
+## Investigate wet/smoked fish
 
-npm_hfss_prod_names_no_dups %>%
-  filter(Market == "Non Alcoholic Beer") %>%
-  group_by(HFSS) %>%
-  tally() %>%
-  ungroup()
-# Only 8 are HFSS. They are all fruit-flavoured ones (presumably higher in sugar).
+npm_markets %>%
+  filter(market == "Wet/Smoked Fish") %>%
+  group_by(sub_market) %>%
+  tally()
