4ad70513754832ac467d6ad33080c6d2f27924b6
Elaine <elaine.tod@phs.scot>
2024-09-17 10:44:30 +0100

    I tidied up the code descriptors/info to help with QA

--- Code/SG Scottish manufacturer analysis Sep24.R
+++ Code/SG Scottish manufacturer analysis Sep24.R
@@ -7,103 +7,87 @@
 #Set libraries and working directories
 library(tidyverse) #lots of functions
 library(arrow) # for efficient file saving and reading
+library(tidyr)
 library(dplyr)
 library(readr)
 library(data.table) # For 'fread' function to read in CSVs efficiently
 setwd("/PHI_conf/PHSci-HFSS/Kantar analysis/Working Data/")
 
 
+###########################################################################
+#PART 1:CREATE SG DATASET 2023 (PURCHASES WITH MANUFACTURER AND HFSS INFO)#
+###########################################################################
 
-#PART 1:LOOK AT THE DISTRIBUTION OF TOTAL HFSS SALES BY MANUFACTURER IN 2023
+HFSSFINAL23 <- read_parquet("/PHI_conf/PHSci-HFSS/Kantar analysis/Working Data/HFSSFINAL23.parquet")#Open linked data file for 2023
 
-#Open linked data file for 2023
-HFSSFINAL23 <- read_parquet("/PHI_conf/PHSci-HFSS/Kantar analysis/Working Data/HFSSFINAL23.parquet")
+files <- list.files(path = "2023 Purchase Data/product master", full.names = TRUE)#Create combined RFfile
 
-#Link manufacturer data to HFSSFINAL22_23 data file uing Catriona's 'rf_markets_combined.parquet file
-rf_markets_combined <- read_parquet("/PHI_conf/PHSci-HFSS/Kantar analysis/Working Data/rf_markets_combined.parquet")
+dataframes <- lapply(files, function(f){
+  fread(f, skip = 1, header = TRUE) %>%
+    select(any_of(c("Product", "Product Desc", "Manufacturer", "Brand", "RST 4 Market", "RST 4 Sub Market", "RST 4 Extended", "Low Sugar/Calorie/Fat")))
+}
+)
+
+rf_combined_SG <- do.call(bind_rows, dataframes)
+
+write_parquet(rf_combined_SG, "rf_combined_SG.parquet")
+
+
+rf_combined_SG <- read_parquet("/PHI_conf/PHSci-HFSS/Kantar analysis/Working Data/rf_combined_SG.parquet")#Link manufacturer data to HFSSFINAL22_23 data file using a version of Catriona's 'rf_markets_combined.parquet file - saved separately.
+
+
+#QA - remove 47 products with missing manufacturer info
+rf_combined_SG <- rf_combined_SG[-which(rf_combined_SG$Manufacturer == ""), ]
 
 #Join panel and purchase data with NPM and HFSS data (NPM_2024P2)
-SG_dataset <- dplyr::left_join(HFSSFINAL23, rf_markets_combined, by=c("prodcode" = "Product"))
+SG_dataset <- dplyr::left_join(HFSSFINAL23, rf_combined_SG, by=c("prodcode" = "Product"))
 
 
-#TEMPORARY CODE - Change amtspent to numeric
+#Change amtspent to numeric
 SG_dataset$amtspent <- as.numeric(SG_dataset$amtspent)
+SG_dataset <- SG_dataset %>% mutate_at(c('amtspent'), as.numeric)
+
 
 #QA CHECK
 #2,421,692 observations of 54 variables
+
+#Drop non-HFSS products
+SG_dataset <- SG_dataset[SG_dataset$HFSS != 0, ]
 
 #Write data file for SG analysis
 write_parquet(SG_dataset, "SG_dataset.parquet")
 
 
-################################
-#SYNTAX TO CREATE TABLE METRICS#
-################################
+########################################
+#PART 2: SYNTAX TO CREATE TABLE METRICS#
+#######################################
 
-#START HERE 11/09/24
 
 #Open SG data file
 SG_dataset <- read_parquet("/PHI_conf/PHSci-HFSS/Kantar analysis/Working Data/SG_dataset.parquet")
 
-#Create table showing total amount spent by manufacturer for all HFSS products 
-SGTableA <- SG_dataset %>%
-  group_by(Manufacturer) %>%
-  summarise (Total_spend = sum(amtspent[`HFSS` == 1])) %>%
-  view
-
-#1,967 valid manufacturers
-#1 which is NA - remove
+#TEMP CODE QA
+#SG_dataset <- SG_dataset %>%
+  #filter(Manufacturer == "108 Foods Sarl" )
 
 
+#SUMMARY STATISTICS
+SGTableA = SG_dataset%>%        
+  group_by(Manufacturer) %>% 
+  summarise(Spend= sum(amtspent [`HFSS` == 1]))#Create table showing total amount spent on HFSS products in 2023,  by manufacturer
 
-#Create variable to show percentage of total purchases (monetary pounds) for each of the 1,967 manufacturers in 2023
-  mutate(percentage = Total_spend / sum(Total_spend) * 100) %>%
-  view
+summary(SGTableA)
 
-#Create cumulative percentage variable to help with cut off of manufacturers (Y%)
+SGTableA_clean = SGTableA %>% drop_na(Spend) # remove one blank manufacturer
 
+summary(SGTableA_clean)
+            
+SGTableA_clean = SGTableA_clean %>% 
+  mutate(percent = (Spend/sum(Spend)*100))#Calculate percentage of total spend by manufacturer SGTableA %>%
 
-  
-  
+SGTableA_clean <- SGTableA_clean %>% #Calculate cumulative percentage of total spend by manufacturer
+  arrange(-Spend) %>% 
+  mutate(cumperc=cumsum(percent))
 
-
- 
-
-
-  write_csv(TableA, filename="SGTableA.csv")
-                                         
-summarydf <- phenology %>% group_by......(as above)
-
-write.csv(summarydf, filename="yourfilenamehere.csv")          
-                                         
-
-
-
-
-
-prob = c(0.25, 0.75))
-group_by(SG_dataset, Manufacturer) %>% mutate(percent = amtspent/sum(amtspent))
-  
-  
-SG_dataset %>%
-  group_by(Manufacturer) %>%
-  mutate(Perc_total_HFSS_sales = amtspent / sum(amtspent) * 100)
-  
-  
-
-
-
-#Create bar chart showing:
-# x axis manufacturers
-# y axis total monetary sales
-# restrict to HFSS = 1
-
-SG_dataset %>%
-  filter(HFSS == 0)
-ggplot(SG_dataset, aes(x=Manufacturer, y=amtspent))
-  
-  
-  ggplot(SG_datast,aes(Manufacturer,amtspent))+
-  {if(Switch)geom_hline(yintercept=15)}+
-  geom_point() 
-
+#Write CSV file for charts in Excel
+write_csv(SGTableA_clean, "SGTableA_clean.csv")
